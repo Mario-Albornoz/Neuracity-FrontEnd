89712391ed2c410ecc056a1b3e3431b266b74493
TanyLM04 <91760744+TanyLM04@users.noreply.github.com>
2025-03-02 07:15:42 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import Creator from "./Creator";
 import Classroom from "./Classroom";
 import Support from "./Support";
 import Quizzes from "./Quizzes";
+import CoursePlanner from "./CoursePlanner";
 
-export {Home, Creator, Classroom, Support, Quizzes}
+export {Home, Creator, Classroom, Support, Quizzes, CoursePlanner}
